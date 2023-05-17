7afdf6d02cb8d58e9c2ba4450c3e3e1cf113df3c
MahmoudRezk1 <mahmoudrezk514@gmail.com>
2023-05-17 17:05:51 +0300

    added audit config

--- src/main/java/com/shopping/base/entity/BaseEntity.java
+++ src/main/java/com/shopping/base/entity/BaseEntity.java
@@ -1,15 +1,13 @@
 package com.shopping.base.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -17,6 +15,7 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity <ID extends Number>{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
